97ea3c65b0b853089e3e806ea1d7a03a586cff17
Your Name <you@example.com>
2024-11-21 15:54:48 +0900

    Commonize templates, partials, and CSS

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("signup", views.user_new, name="user_new")
+  path("signup", views.user_new, name="user_new"),
+  path("user/<int:pk>", views.user_show, name="user_show")
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 def user_new(request):
   return render(request, "user/new.html")
+
+def user_show(request, pk):
+  user = get_object_or_404(User, pk=pk)
+  return render(request, "user/show.html", {"user": user})
